6afc7c4e4a9e8c001d34c0dda757a7a00a8a0311
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-04-09 09:47:31 +0200

    fixed some issues

---
+++ src/lang/flybytes/tests/examples/Trivial.java
@@ -0,0 +1,9 @@
+package lang.flybytes.tests.examples;
+
+public class Trivial {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
